8815eefb1f8614e61582d21902d35b2677e02dc0
Akshay Sharma <akshay.fi@outlook.com>
2019-03-06 20:47:55 +0200

    adding project to github

--- src/App.js
+++ src/App.js
@@ -3,23 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {description: '', items: []}
+  }
+  //first created inputChange function below then defined here
+  inputChange = (event) => {
+    this.setState({description: event.target.value})
+  }
+
+  submitForm = (event) => {
+    event.preventDefault();
+    this.setState({items: [...this.state.items, this.state.description] })
+  }
+
+
   render() {
+
+    const todos = this.state.items.map((todo, index) => <tr key={index}><td>{todo}</td></tr>)
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+          Todo list
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
+        <div>
+
+        <form onSubmit={this.submitForm}>
+          <input type="text" value={this.state.description} onChange={this.inputChange} />
+          <input type="submit" value="Add" />
+        </form>
+
+        </div>
+
+        <div>
+          <table><tbody>{todos}</tbody></table>
+        </div>
+
       </div>
     );
   }
